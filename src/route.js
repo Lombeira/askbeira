6fab5a7551b278a24740e551a2336f354dca9b07
Lombeira <glomenha@gmail.com>
2021-06-24 00:27:28 -0300

    Commit Bomba

--- src/route.js
+++ src/route.js
@@ -2,5 +2,10 @@ const express = require("express");
 const route = express.Router();
 
 route.get("/", (req, res) => res.render("index"));
+route.get("/room", (req, res) => res.render("room"));
+route.get("/create-pass", (req, res) => res.render("create-pass"));
+//route.get("/room/:room/:question/:action", (req, res) =>
+//  res.render("exemplo", { req })
+//);
 
 module.exports = route;
