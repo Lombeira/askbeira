e4e0f99d2dcf2cbf2fb33cf1c440129ce025ac10
Lombeira <glomenha@gmail.com>
2021-06-24 23:27:36 -0300

    Back-End

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,15 +1,20 @@
 import Modal from "./modal.js";
 
 const modal = Modal();
+
 const modalTitle = document.querySelector(".modal h2");
 const modalDescription = document.querySelector(".modal p");
 const modalButton = document.querySelector(".modal button");
+
+//Pegar todos os botões que existe com a classe check
 const checkButtons = document.querySelectorAll(".actions a.check");
 
 checkButtons.forEach((button) => {
+  //adicionar a escuta
   button.addEventListener("click", handleClick);
 });
 
+/*Quando o botão delete for clicado ele abre a modal */
 const deleteButton = document.querySelectorAll(".actions a.delete");
 
 deleteButton.forEach((button) => {
@@ -20,15 +25,19 @@ function handleClick(event, check = true) {
   event.preventDefault();
   const text = check ? "Marcar como lida" : "Excluir";
   const slug = check ? "check" : "delete";
-  const roomId = document.querySelector("#room-id").dataset("id");
+  const roomId = document.querySelector("#room-id").dataset.id;
   const questionId = event.target.dataset.id;
+
   const form = document.querySelector(".modal form");
-  form.setAttribute("action", `/room/${roomId}/${questionId}/${slug}`);
+  form.setAttribute("action", `/question/${roomId}/${questionId}/${slug}`);
+
   modalTitle.innerHTML = `${text} esta pergunta`;
   modalDescription.innerHTML = `Tem certeza que deseja ${text.toLowerCase()} esta pergunta?`;
-  modalButton.innerHTML = `Sim,${text.toLowerCase()}`;
+  modalButton.innerHTML = `Sim, ${text.toLowerCase()}`;
   check
     ? modalButton.classList.remove("red")
     : modalButton.classList.add("red");
+
+  //abrir modal
   modal.open();
 }
--- src/route.js
+++ src/route.js
@@ -1,11 +1,18 @@
 const express = require("express");
+const QuestionController = require("./controllers/QuestionController");
+const RoomController = require("./controllers/RoomController");
+
 const route = express.Router();
 
-route.get("/", (req, res) => res.render("index"));
-route.get("/room", (req, res) => res.render("room"));
-route.get("/create-pass", (req, res) => res.render("create-pass"));
-//route.get("/room/:room/:question/:action", (req, res) =>
-//  res.render("exemplo", { req })
-//);
+route.get("/", (req, res) => res.render("index", { page: "enter-room" }));
+route.get("/create-pass", (req, res) =>
+  res.render("index", { page: "create-pass" })
+);
+
+route.get("/room/:room", (req, res) => res.render("room"));
+
+// Formato que o formulario de dentro da modal tem que passar a informação
+route.post("/question/:room/:question/:action", QuestionController.index);
+route.post("/create-room", RoomController.create);
 
 module.exports = route;
