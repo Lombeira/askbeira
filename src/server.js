3f5a1a9669775a5f57b1c90e2d57f97451987dc4
Lombeira <glomenha@gmail.com>
2021-08-11 13:20:56 -0300

    heroku

--- src/server.js
+++ src/server.js
@@ -1,17 +1,18 @@
-const express = require("express");
-const route = require("./route");
-const path = require("path");
-
+const express = require('express');
+const route = require('./route');
+const path = require('path');
 const server = express();
 
-server.set("view engine", "ejs");
+const port = process.env.PORT || 3000;
 
-server.use(express.static("public"));
+server.set('view engine', 'ejs');
 
-server.set("views", path.join(__dirname, "views"));
+server.use(express.static('public'));
+
+server.set('views', path.join(__dirname, 'views'));
 
 server.use(express.urlencoded({ extended: true }));
 
 server.use(route);
 
-server.listen(3000, () => console.log("RODANDO"));
+server.listen(port, () => console.log('Aplicação Rodando'));
